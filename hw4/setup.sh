36cadf5fcc2e80d76a80c6360c147a177bf94c6f
sanjask11 <sanjask@bu.edu>
2026-03-05 16:21:31 -0500

    Update setup.sh

--- hw4/setup.sh
+++ hw4/setup.sh
@@ -21,6 +21,7 @@ SUBSCRIPTION="forbidden-requests-sub"
 
 SERVER_VM="hw4-server"
 REPORTER_VM="hw4-reporter"
+CLIENT_VM="hw4-client"
 
 SERVER_SA="hw4-server-sa"
 REPORTER_SA="hw4-reporter-sa"
@@ -104,6 +105,18 @@ gcloud compute instances create "${REPORTER_VM}" \
   --scopes="https://www.googleapis.com/auth/cloud-platform" \
   --metadata="APP=service2,PROJECT_ID=${PROJECT_ID},REPO_URL=${REPO_URL},TOPIC=${TOPIC},SUBSCRIPTION=${SUBSCRIPTION}" \
   --metadata-from-file startup-script="startup.sh" \
+  >/dev/null 2>&1 || true
+
+echo "Creating client VM..."
+gcloud compute instances create "${CLIENT_VM}" \
+  --zone="${ZONE}" \
+  --machine-type="e2-micro" \
+  --image-family="debian-12" --image-project="debian-cloud" \
+  --tags="hw4-client" \
+  --metadata=startup-script='#! /bin/bash
+apt-get update -y
+apt-get install -y python3 git
+' \
   >/dev/null 2>&1 || true
 
 echo "DONE"
