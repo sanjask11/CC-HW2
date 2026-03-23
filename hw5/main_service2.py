8e3603e804512fb0c04ac575a618665d5d2deba8
sanjask11 <sanjask@bu.edu>
2026-03-23 17:41:51 -0400

    Update main_service2.py

--- hw5/main_service2.py
+++ hw5/main_service2.py
@@ -2,28 +2,29 @@
 import os
 import sys
 import time
+
 from google.cloud import pubsub_v1
 
 PROJECT_ID = os.environ.get("PROJECT_ID", "")
-SUBSCRIPTION = "forbidden-requests-sub"  
+SUBSCRIPTION = os.environ.get("SUBSCRIPTION", "")
 
 def main():
-    if not PROJECT_ID:
-        print("Missing PROJECT_ID", file=sys.stderr)
+    if not PROJECT_ID or not SUBSCRIPTION:
+        print("Missing PROJECT_ID or SUBSCRIPTION", file=sys.stderr)
         sys.exit(1)
-    
+
     subscriber = pubsub_v1.SubscriberClient()
     sub_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION)
-    
+
     def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         try:
             print(message.data.decode("utf-8", errors="replace"), flush=True)
         finally:
             message.ack()
-    
+
     streaming_pull_future = subscriber.subscribe(sub_path, callback=callback)
     print(f"Listening on {sub_path}", flush=True)
-    
+
     try:
         streaming_pull_future.result()
     except KeyboardInterrupt:
